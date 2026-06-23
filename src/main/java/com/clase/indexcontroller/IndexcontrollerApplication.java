622eb1921d5ee68997d3cdb8459af97d511fbf4e
José Cerezo <joseenrique6161@gmail.com>
2026-06-22 21:30:42 -0500

    Created project

---
+++ src/main/java/com/clase/indexcontroller/IndexcontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.clase.indexcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexcontrollerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clase/indexcontroller/IndexcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clase.indexcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
