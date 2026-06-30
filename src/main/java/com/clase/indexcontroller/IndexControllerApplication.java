fc976147d61380b86b4e6db0c379a5fb65889505
José Cerezo <joseenrique6161@gmail.com>
2026-06-29 20:01:46 -0500

    Added IndexControllerApplication

---
+++ src/main/java/com/clase/indexcontroller/IndexControllerApplication.java
@@ -0,0 +1,11 @@
+package com.clase.indexcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexControllerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(IndexControllerApplication.class, args);
+  }
+}
